4faae15cf5d13c8c9216b0179765d3f870f142db
mechztcc <dojathan@gmail.com>
2022-03-28 11:23:44 -0300

    fix: added scroll to login page

--- lib/app/modules/user/pages/login_page.dart
+++ lib/app/modules/user/pages/login_page.dart
@@ -55,80 +55,82 @@ class LoginPageState extends ModularState<LoginPage, UserStore> {
                     )
                     : Form(
                         key: _formKey,
-                        child: Column(
-                          children: [
-                            const Text(
-                              'Bem-vindo novamente',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              const Text(
+                                'Bem-vindo novamente',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                ),
                               ),
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            CustomInputWidget(
-                              controller: emailEC,
-                              validator: Validatorless.multiple([
-                                Validatorless.email('E-mail inválido'),
-                                Validatorless.required('E-mail é obrigatório')
-                              ]),
-                              icon: const Icon(Icons.person),
-                              label: 'E-mail',
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            CustomInputWidget(
-                              label: 'Senha',
-                              icon: const Icon(Icons.lock),
-                              validator: Validatorless.multiple([
-                                Validatorless.required('Senha é obrigatório'),
-                                Validatorless.min(6,
-                                    'A senha deve conter no mínimo 6 caracteres')
-                              ]),
-                              controller: passwordEC,
-                            ),
-                            SizedBox(
-                              width: constraints.maxWidth * 0.8,
-                              child: GradientButtonWidget(
-                                func: validateForm,
-                                label: 'Entrar',
-                                gradient: const [
-                                  Color(0xffFB6440),
-                                  Color(0xffFBAF40),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              CustomInputWidget(
+                                controller: emailEC,
+                                validator: Validatorless.multiple([
+                                  Validatorless.email('E-mail inválido'),
+                                  Validatorless.required('E-mail é obrigatório')
+                                ]),
+                                icon: const Icon(Icons.person),
+                                label: 'E-mail',
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              CustomInputWidget(
+                                label: 'Senha',
+                                icon: const Icon(Icons.lock),
+                                validator: Validatorless.multiple([
+                                  Validatorless.required('Senha é obrigatório'),
+                                  Validatorless.min(6,
+                                      'A senha deve conter no mínimo 6 caracteres')
+                                ]),
+                                controller: passwordEC,
+                              ),
+                              SizedBox(
+                                width: constraints.maxWidth * 0.8,
+                                child: GradientButtonWidget(
+                                  func: validateForm,
+                                  label: 'Entrar',
+                                  gradient: const [
+                                    Color(0xffFB6440),
+                                    Color(0xffFBAF40),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                width: constraints.maxWidth * 0.8,
+                                child: GradientButtonWidget(
+                                  func: () {
+                                    Modular.to.pushNamed('/');
+                                  },
+                                  label: 'Criar conta gratuita',
+                                  gradient: const [
+                                    Color(0xff5F72E4),
+                                    Color(0xff805EE4),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: const [
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Expanded(
+                                    child: Divider(),
+                                  ),
                                 ],
                               ),
-                            ),
-                            SizedBox(
-                              width: constraints.maxWidth * 0.8,
-                              child: GradientButtonWidget(
-                                func: () {
-                                  Modular.to.pushNamed('/');
-                                },
-                                label: 'Criar conta gratuita',
-                                gradient: const [
-                                  Color(0xff5F72E4),
-                                  Color(0xff805EE4),
-                                ],
+                              const Text(
+                                'Ao entrar você concorda com os Termos de acesso & Privacidade',
+                                style: TextStyle(color: Colors.black54),
+                                textAlign: TextAlign.center,
                               ),
-                            ),
-                            Row(
-                              children: const [
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Expanded(
-                                  child: Divider(),
-                                ),
-                              ],
-                            ),
-                            const Text(
-                              'Ao entrar você concorda com os Termos de acesso & Privacidade',
-                              style: TextStyle(color: Colors.black54),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
               ),
