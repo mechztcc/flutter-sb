2e7d28b56995cba8cd5a9fe1ecb733ddde7496ce
mechztcc <dojathan@gmail.com>
2022-04-06 14:52:34 -0300

    fix: product details

--- lib/app/modules/product/pages/product_details_page.dart
+++ lib/app/modules/product/pages/product_details_page.dart
@@ -32,82 +32,84 @@ class ProductDetailsPageState extends State<ProductDetailsPage> {
       body: LayoutBuilder(
         builder: ((context, constraints) => Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset('assets/burguer2.jpg'),
-                  ),
-                  Row(
-                    children: const [
-                      Icon(
-                        Icons.star_rounded,
-                        color: Color(0xffFB6440),
-                        size: 30,
-                      ),
-                      Icon(
-                        Icons.star_rounded,
-                        color: Color(0xffFB6440),
-                        size: 30,
-                      ),
-                      Icon(
-                        Icons.star_rounded,
-                        color: Color(0xffFB6440),
-                        size: 30,
-                      ),
-                      Icon(
-                        Icons.star_rounded,
-                        color: Color(0xffFB6440),
-                        size: 30,
-                      ),
-                      Icon(
-                        Icons.star_border_rounded,
-                        color: Color(0xffFB6440),
-                        size: 30,
-                      ),
-                      Text('(225)', style: TextStyle(color: Color(0xffFB6440)))
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      Text(
-                        'Descrição',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RichText(
-                        text: const TextSpan(
-                            text: 'R\$ 22.00',
-                            style: TextStyle(
-                                color: Color(0xffFB6440),
-                                fontSize: 35,
-                                fontWeight: FontWeight.bold)),
-                      ),
-                      SizedBox(
-                        width: constraints.maxWidth * 0.5,
-                        child: GradientButtonWidget(
-                          label: 'Adicionar',
-                          gradient: gradient,
-                          func: () {},
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset('assets/burguer2.jpg'),
+                    ),
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.star_rounded,
+                          color: Color(0xffFB6440),
+                          size: 30,
                         ),
-                      )
-                    ],
-                  )
-                ],
+                        Icon(
+                          Icons.star_rounded,
+                          color: Color(0xffFB6440),
+                          size: 30,
+                        ),
+                        Icon(
+                          Icons.star_rounded,
+                          color: Color(0xffFB6440),
+                          size: 30,
+                        ),
+                        Icon(
+                          Icons.star_rounded,
+                          color: Color(0xffFB6440),
+                          size: 30,
+                        ),
+                        Icon(
+                          Icons.star_border_rounded,
+                          color: Color(0xffFB6440),
+                          size: 30,
+                        ),
+                        Text('(225)', style: TextStyle(color: Color(0xffFB6440)))
+                      ],
+                    ),
+                    Column(
+                      children: const [
+                        Text(
+                          'Descrição',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RichText(
+                          text: const TextSpan(
+                              text: 'R\$ 22.00',
+                              style: TextStyle(
+                                  color: Color(0xffFB6440),
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        SizedBox(
+                          width: constraints.maxWidth * 0.5,
+                          child: GradientButtonWidget(
+                            label: 'Adicionar',
+                            gradient: gradient,
+                            func: () {},
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             )),
       ),
